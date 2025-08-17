a8fce83ae3851f2268b0305e3e2705901b78e077
bayesOnTheWeb <steevn.pyram@gmail.com>
2025-08-17 17:19:03 +0200

    formattage

--- src/utils/services/createUser.ts
+++ src/utils/services/createUser.ts
@@ -2,27 +2,27 @@ import prisma from '../../../lib/prisma'
 import { hashPassword } from './hashPassword'
 
 interface UserInput {
-  username: string
-  password: string
-  email: string
+    username: string
+    password: string
+    email: string
 }
 
 export async function createUser(user: UserInput) {
-  try {
-    const hashedPassword = await hashPassword({ password: user.password })
+    try {
+        const hashedPassword = await hashPassword({ password: user.password })
 
-    const addedUser = await prisma.user.create({
-      data: {
-        username: user.username,
-        email: user.email,
-        password: hashedPassword,
-        role: { connect: { type: 'USER' } }, // obligatoire
-      },
-    })
+        const addedUser = await prisma.user.create({
+            data: {
+                username: user.username,
+                email: user.email,
+                password: hashedPassword,
+                role: { connect: { type: 'USER' } }, // obligatoire
+            },
+        })
 
-    return addedUser
-  } catch (error) {
-    console.error('Erreur création utilisateur :', error)
-    throw new Error("Échec lors de la persistance de l'utilisateur en DB")
-  }
+        return addedUser
+    } catch (error) {
+        console.error('Erreur création utilisateur :', error)
+        throw new Error("Échec lors de la persistance de l'utilisateur en DB")
+    }
 }
--- src/utils/services/defaultRole.ts
+++ src/utils/services/defaultRole.ts
@@ -1,22 +1,22 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 async function main() {
-  // Vérifie si le rôle USER existe déjà
-  const existingRole = await prisma.role.findUnique({
-    where: { type: 'USER' },
-  });
+    // Vérifie si le rôle USER existe déjà
+    const existingRole = await prisma.role.findUnique({
+        where: { type: 'USER' },
+    })
 
-  if (!existingRole) {
-    await prisma.role.create({
-      data: { type: 'USER' },
-    });
-    console.log('Rôle USER créé');
-  } else {
-    console.log('Rôle USER existe déjà');
-  }
+    if (!existingRole) {
+        await prisma.role.create({
+            data: { type: 'USER' },
+        })
+        console.log('Rôle USER créé')
+    } else {
+        console.log('Rôle USER existe déjà')
+    }
 }
 
 main()
-  .catch(e => console.error(e))
-  .finally(() => prisma.$disconnect());
+    .catch((e) => console.error(e))
+    .finally(() => prisma.$disconnect())
